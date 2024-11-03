adb84fe9d9ac8d699871fac88f9b88fb72696505
wwang93 <142034236+wwang93@users.noreply.github.com>
2024-11-03 17:20:21 -0500

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -2,12 +2,12 @@ library(learnr)
 library(shiny)
 library(rsconnect)
 
-# 设置 ShinyApps.io 账户信息
+# set up  ShinyApps.io  self account info
 rsconnect::setAccountInfo(name='vinowang',
                           token='C446F2D73BA9A05D63F7B4C09BB42D02',
                           secret='Eym/FRvOWl11Nc46gCglw969+CWDQUnVxF159NaK')
 
-# 部署应用，确保包含所有必要的文件
+# deploy your app
 deployApp(appDir = ".", 
           appFiles = c("Intro to Data Science.Rmd", "March_Madness_Data.csv", "images"))
 
